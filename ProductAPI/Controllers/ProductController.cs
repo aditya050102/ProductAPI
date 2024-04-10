395961b1fdc525a019355d26518c3447d0bd83e6
Aditya Anurag <aditya.anurag@nagarro.com>
2024-04-10 12:53:21 +0530

    Added Certain Changes

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using ProductAPI.Models;
 using ProductAPI.Service;
 
@@ -6,42 +9,226 @@ namespace ProductAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : Controller
+    public class ProductController : ControllerBase
     {
-        public ProductService productService { get; set; }
+        private readonly ProductService productService;
+
         public ProductController(ProductService productService)
         {
-            this.productService = productService;
+            this.productService = productService ?? throw new ArgumentNullException(nameof(productService));
         }
 
         [HttpPost("addProduct")]
         public async Task<ActionResult<Products>> AddAsync(Products product)
         {
-            return await productService.AddAsync(product);
+            if (product == null)
+            {
+                return BadRequest("Product data is null");
+            }
+
+            var addedProduct = await productService.AddAsync(product);
+
+            return Ok(addedProduct);
         }
 
         [HttpGet("deleteProduct")]
         public async Task<ActionResult<Products>> DeleteAsync(Guid id)
         {
-            return await productService.DeleteAsync(id);
+            if (id == Guid.Empty)
+            {
+                return BadRequest("Invalid product ID");
+            }
+
+            var deletedProduct = await productService.DeleteAsync(id);
+
+            if (deletedProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(deletedProduct);
         }
 
         [HttpGet("getAllProducts")]
-        public async Task<IEnumerable<Products>> GetAllAsync()
+        public async Task<ActionResult<IEnumerable<Products>>> GetAllAsync()
         {
-            return await productService.GetAllAsync();
+            var products = await productService.GetAllAsync();
+
+            return Ok(products);
         }
 
         [HttpGet("getProduct")]
         public async Task<ActionResult<Products>> GetAsync(Guid id)
         {
-            return await productService.GetAsync(id);
+            if (id == Guid.Empty)
+            {
+                return BadRequest("Invalid product ID");
+            }
+
+            var product = await productService.GetAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
         }
 
         [HttpPut("updateProduct")]
         public async Task<ActionResult<Products>> UpdateAsync(Products product)
         {
-            return await productService.UpdateAsync(product);
+            if (product == null)
+            {
+                return BadRequest("Product data is null");
+            }
+
+            var updatedProduct = await productService.UpdateAsync(product);
+
+            if (updatedProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedProduct);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using Microsoft.AspNetCore.Mvc;
+//using ProductAPI.Models;
+//using ProductAPI.Service;
+
+//namespace ProductAPI.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ProductController : Controller
+//    {
+//        public ProductService productService { get; set; }
+//        public ProductController(ProductService productService)
+//        {
+//            this.productService = productService;
+//        }
+
+//        [HttpPost("addProduct")]
+//        public async Task<ActionResult<Products>> AddAsync(Products product)
+//        {
+//            return await productService.AddAsync(product);
+//        }
+
+//        [HttpGet("deleteProduct")]
+//        public async Task<ActionResult<Products>> DeleteAsync(Guid id)
+//        {
+//            return await productService.DeleteAsync(id);
+//        }
+
+//        [HttpGet("getAllProducts")]
+//        public async Task<IEnumerable<Products>> GetAllAsync()
+//        {
+//            return await productService.GetAllAsync();
+//        }
+
+//        [HttpGet("getProduct")]
+//        public async Task<ActionResult<Products>> GetAsync(Guid id)
+//        {
+//            return await productService.GetAsync(id);
+//        }
+
+//        [HttpPut("updateProduct")]
+//        public async Task<ActionResult<Products>> UpdateAsync(Products product)
+//        {
+//            return await productService.UpdateAsync(product);
+//        }
+//    }
+//}
--- ProductAPI/Service/ProductService.cs
+++ ProductAPI/Service/ProductService.cs
@@ -1,41 +1,45 @@
-﻿using ProductAPI.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProductAPI.Models;
 
 namespace ProductAPI.Service
-
 {
     public class ProductService
-
     {
-        public List<Products> productsContainer = new List<Products>();
-        public  async Task<Products> AddAsync(Products product)
+        private readonly List<Products> productsContainer = new List<Products>();
+
+        public async Task<Products?> AddAsync(Products product)
         {
             product.Id = Guid.NewGuid();
             productsContainer.Add(product);
-            return product;
+            return await Task.FromResult(product);
         }
 
-        public async Task<Products> DeleteAsync(Guid id)
+        public async Task<Products?> DeleteAsync(Guid id)
         {
             var existingProduct = productsContainer.Find(x => x.Id == id);
 
-            productsContainer.Remove(existingProduct);
+            if (existingProduct != null)
+            {
+                productsContainer.Remove(existingProduct);
+            }
 
-            return existingProduct;
+            return await Task.FromResult(existingProduct);
         }
 
         public async Task<IEnumerable<Products>> GetAllAsync()
         {
-            return productsContainer;
+            return await Task.FromResult(productsContainer);
         }
 
-        public async Task<Products> GetAsync(Guid id)
+        public async Task<Products?> GetAsync(Guid id)
         {
-            return productsContainer.Find(x => x.Id == id);
+            return await Task.FromResult(productsContainer.Find(x => x.Id == id));
         }
 
-        public async Task<Products> UpdateAsync(Products product)
+        public async Task<Products?> UpdateAsync(Products product)
         {
-
             var existingProduct = productsContainer.Find(x => x.Id == product.Id);
 
             if (existingProduct != null)
@@ -48,11 +52,130 @@ namespace ProductAPI.Service
                 existingProduct.Discount = product.Discount;
                 existingProduct.Specification = product.Specification;
 
-                return product;
+                return await Task.FromResult(existingProduct);
             }
 
-            return null;
+            return await Task.FromResult<Products?>(null);
         }
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using ProductAPI.Models;
+
+//namespace ProductAPI.Service
+
+//{
+//    public class ProductService
+
+//    {
+//        public List<Products> productsContainer = new List<Products>();
+//        public  async Task<Products> AddAsync(Products product)
+//        {
+//            product.Id = Guid.NewGuid();
+//            productsContainer.Add(product);
+//            return product;
+//        }
+
+//        public async Task<Products> DeleteAsync(Guid id)
+//        {
+//            var existingProduct = productsContainer.Find(x => x.Id == id);
+
+//            productsContainer.Remove(existingProduct);
+
+//            return existingProduct;
+//        }
+
+//        public async Task<IEnumerable<Products>> GetAllAsync()
+//        {
+//            return productsContainer;
+//        }
+
+//        public async Task<Products> GetAsync(Guid id)
+//        {
+//            return productsContainer.Find(x => x.Id == id);
+//        }
+
+//        public async Task<Products> UpdateAsync(Products product)
+//        {
+
+//            var existingProduct = productsContainer.Find(x => x.Id == product.Id);
+
+//            if (existingProduct != null)
+//            {
+//                existingProduct.Name = product.Name;
+//                existingProduct.Description = product.Description;
+//                existingProduct.Category = product.Category;
+//                existingProduct.Quantity = product.Quantity;
+//                existingProduct.Price = product.Price;
+//                existingProduct.Discount = product.Discount;
+//                existingProduct.Specification = product.Specification;
+
+//                return product;
+//            }
+
+//            return null;
+//        }
+
+//    }
+//}
